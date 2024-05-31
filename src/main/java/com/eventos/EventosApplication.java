588ee214ac97411b04d2c81bf51966ad8f1c6f1d
Jose Leandro Santos Cruz <leandros_cruz@outlook.com>
2024-05-31 19:44:04 -0300

    raw: integrando banco de dados e modificando nome das pastas

--- src/main/java/com/eventos/EventosApplication.java
+++ src/main/java/com/eventos/EventosApplication.java
@@ -1,13 +1,13 @@
-package com.agenda;
+package com.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgendaApplication {
+public class EventosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AgendaApplication.class, args);
+		SpringApplication.run(EventosApplication.class, args);
 	}
 
 }
